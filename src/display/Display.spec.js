1ee904727b7284c356350f2867f1312ad20ed233
Irving Duran <ijd.irving@gmail.com>
2019-05-15 17:05:07 -0300

    mvp

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -5,34 +5,58 @@ import Display from "./Display";
 import { render } from "react-testing-library";
 import "react-testing-library/cleanup-after-each";
 
-describe("open door open lock", () => {
-  const component = render(<Display locked={false} closed={false} />);
+// describe("open door open lock", () => {
+//   const conditionOne = render(<Display locked={false} closed={false} />);
 
-  it("door led", () => {
-    const displayDoor = component.getByText("Open");
+//   it.skip("door open led", () => {
+//     const displayOpenDoor = conditionOne.getByText("Open");
 
-    expect(displayDoor).toHaveClass("green-led");
+//     expect(displayOpenDoor).toHaveClass("green-led");
+//   });
+
+//   it.skip("unlocked led", () => {
+//     const displayLock = conditionOne.getByText("Unlocked");
+
+//     expect(displayLock).toHaveClass("green-led");
+//   });
+// });
+
+describe("close door and lock", () => {
+  it("door closed led", () => {
+    expect(
+      render(<Display locked={true} closed={true} />).getByText("Closed")
+    ).toHaveClass("red-led");
   });
+});
 
-  it("lock led when door and lock are open", () => {
-    const displayLock = component.getByText("Unlocked");
-
-    expect(displayLock).toHaveClass("green-led");
+describe("door locked", () => {
+  it("door locked led", () => {
+    expect(
+      render(<Display locked={true} closed={true} />).getByText("Locked")
+    ).toHaveClass("red-led");
   });
 });
 
-describe("close door open lock", () => {
-  const component = render(<Display locked={false} closed={true} />);
-
-  it("door led", () => {
-    const displayDoor = component.getByText("Closed");
-
-    expect(displayDoor).toHaveClass("red-led");
-  });
-
-  it("lock led when door is closed but lock is open", () => {
-    const displayLock = component.getByText("Unlocked");
-
-    expect(displayLock).toHaveClass("green-led");
+describe("door is opened", () => {
+  it("door open led", () => {
+    expect(
+      render(<Display locked={false} closed={false} />).getByText("Open")
+    ).toHaveClass("green-led");
   });
 });
+
+describe("door is unlocked", () => {
+  it("door unlocked led", () => {
+    expect(
+      render(<Display locked={false} closed={false} />).getByText("Unlocked")
+    ).toHaveClass("green-led");
+  });
+});
+
+describe("door is unlocked but closed", () => {
+    it("door unlocked led", () => {
+      expect(
+        render(<Display locked={false} closed={true} />).getByText("Closed")
+      ).toHaveClass("red-led");
+    });
+  });
